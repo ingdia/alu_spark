72a8a700ff159914d9d41aca2c763d700554c2a3
ingdia <d.ingabire2@alustudent.com>
2026-07-12 07:02:40 +0200

    fix : hid cridentials

--- lib/core/constants/cloudinary_config.dart
+++ lib/core/constants/cloudinary_config.dart
@@ -1,9 +1,9 @@
 class CloudinaryConfig {
-  static const String cloudName = 'alu_spark'; // Replace with your cloud name
-  static const String apiKey = '742871685722392'; // Replace with your API key
-  static const String apiSecret = 'q_sws7E3xgfFYtg9-ywLmBUuok8'; // Replace with your API secret
-  
-  // Upload presets (you'll create these in Cloudinary dashboard)
+  static const String cloudName = String.fromEnvironment('CLOUDINARY_CLOUD_NAME');
+  static const String apiKey = String.fromEnvironment('CLOUDINARY_API_KEY');
+  static const String apiSecret = String.fromEnvironment('CLOUDINARY_API_SECRET');
+
+  // Upload presets (create these in Cloudinary dashboard)
   static const String imageUploadPreset = 'alu_spark_preset';
   static const String cvUploadPreset = 'alu_spark_cv_preset';
 }
